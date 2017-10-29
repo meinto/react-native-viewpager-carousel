e6dfd96ff146ff4d663bdece687cf3b22b506733
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2017-10-29 19:21:41 +0100

    added coveralls support

--- setup-jasmine-env.js
+++
@@ -1,9 +0,0 @@
-var jasmineReporters = require('jasmine-reporters')
-jasmine.VERBOSE = true
-// jasmine.getEnv().addReporter(
-//   new jasmineReporters.JUnitXmlReporter({
-//     consolidateAll: false,
-//     savePath: './test_results/',
-//     filePrefix: 'test-results',
-//   })
-// )
