f79de7ae3cb06aadd821774091c42d75cd59562b
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-01-27 22:34:11 +0100

    Fix tests

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+      '@babel/preset-env',
+      'module:metro-react-native-babel-preset',
+  ],
+  plugins: [
+      '@babel/plugin-proposal-class-properties'
+  ]
+};
